91658b13712950c41f17857b2167fda72e24168a
rlaminseok0824 <xornjsrlaals@gmail.com>
2024-05-08 01:07:47 +0900

    FEAT: add injection ensurlization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fullstack_fe/core/resources/injection/injection.dart';
 import 'package:fullstack_fe/core/services/err_tracking_service.dart';
 import 'package:fullstack_fe/presentation/app.dart';
+import 'package:injectable/injectable.dart';
 
 void main() {
   ErrTrackingService.init(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await configureDependencies(Environment.prod);
     runApp(const MyApp());
   });
 }
