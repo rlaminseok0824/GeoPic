66a40314236f86df1917ee66b6e5a25eae50d4d6
rlaminseok0824 <xornjsrlaals@gmail.com>
2024-05-07 17:23:26 +0900

    FIX: test error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fullstack_fe/main.dart';
+import 'package:fullstack_fe/presentation/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
