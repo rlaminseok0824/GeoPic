ee2634d7e8cde52624e0e31a36fce86f2d46e487
rlaminseok0824 <xornjsrlaals@gmail.com>
2024-06-02 02:44:06 +0900

    FIX: refactoring bottom_sheet

--- lib/presentation/map/view/article_record_bottom_sheet.dart
+++ lib/presentation/map/view/article_record_bottom_sheet.dart
@@ -1,9 +1,8 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:fullstack_fe/core/resources/app_colors.dart';
 import 'package:fullstack_fe/feature/article/models/article_record.dart';
+import 'package:fullstack_fe/presentation/article_view/view/article_screen.dart';
 import 'package:fullstack_fe/presentation/common/bottom_sheet/app_record_bottom_sheet.dart';
-import 'package:go_router/go_router.dart';
 
 class ArticleRecordBottomSheet {
   const ArticleRecordBottomSheet._();
@@ -21,65 +20,7 @@ class ArticleRecordBottomSheet {
                 itemCount: record.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          _buildTitle(context, record[index]),
-                          const SizedBox(height: 24),
-                          _buildImage(record[index]),
-                          const SizedBox(height: 24),
-                        ],
-                      )
-                    ],
-                  );
+                  return ArticleScreen(record: record[index]);
                 }),
           ));
-
-  // body: Column(
-  //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //   children: [
-  //     Column(
-  //       crossAxisAlignment: CrossAxisAlignment.start,
-  //       children: [
-  //         _buildTitle(context, record),
-  //         const SizedBox(height: 24),
-  //         _buildImage(record),
-  //         const SizedBox(height: 24),
-  //       ],
-  //     )
-  //   ],
-  // ));
-
-  static ClipRRect _buildImage(ArticleRecord record) {
-    return ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Container(
-          height: 200,
-          width: double.infinity,
-          color: AppColors.background,
-          child: record.imageUrl == null
-              ? null
-              : Image.network(record.imageUrl!, fit: BoxFit.cover),
-        ));
-  }
-
-  static Row _buildTitle(BuildContext context, ArticleRecord record) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(record.title!,
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        IconButton(
-          icon: const Icon(
-            Icons.close,
-            size: 24,
-          ),
-          onPressed: () => context.pop(),
-        ),
-      ],
-    );
-  }
 }
